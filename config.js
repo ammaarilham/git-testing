09bf701660083efaa3f36dea207688dae7b2a0d1
ammaarilham <ammaarilham2056@gmail.com>
2023-01-02 20:06:00 +0600

    Version 3

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 console.log('config.js')
 console.log('version1')
+console.log('second version')
+console.log('version 3')
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 console.log('index.js')
 console.log('version1')
 console.log('second version')
+console.log('second version')
